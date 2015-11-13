8c69e768f4882ace22ec094aa9f8219507b908dd
Jason Pincin <jason@pincin.com>
2015-11-12 19:48:26 -0500

    leveraging eventuate-chainable

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
-var pre       = require('call-hook/pre'),
-    post      = require('call-hook/post'),
+var chainable = require('eventuate-chainable'),
     isPromise = require('is-promise'),
     onError   = require('on-error')
 
-module.exports = function mkFilteredEventuate (upstreamEventuate, options, filter) {
-    if (typeof options === 'function') {
-        filter  = options
-        options = undefined
-    }
-
-    if (typeof upstreamEventuate.destroyed !== 'function')
-        throw new TypeError('first argument should be a non-basic eventuate')
-
-    options                 = options || {}
-    options.destroyResidual = options.destroyResidual !== undefined ? options.destroyResidual : true
-    options.lazy            = options.lazy !== undefined ? options.lazy : true
-
-    var consuming = false
-
-    var eventuate              = upstreamEventuate.factory(options)
-    eventuate.upstreamConsumer = upstreamConsumer
-    eventuate.consume          = pre(eventuate.consume, addUpstreamConsumer)
-    eventuate.destroy          = post(eventuate.destroy, removeUpstreamConsumer)
-
-    upstreamEventuate.destroyed(eventuate.destroy)
-    upstreamConsumer.removed = upstreamConsumerRemoved
-    if (!options.lazy) addUpstreamConsumer()
-
-    return eventuate
+module.exports = chainable(function (eventuate, options, filter) {
+    return upstreamConsumer
 
     function upstreamConsumer (data) {
         if (filter.length === 2) {
@@ -44,24 +20,7 @@ module.exports = function mkFilteredEventuate (upstreamEventuate, options, filte
         }
     }
 
-    function addUpstreamConsumer () {
-        if (!consuming && !eventuate.isDestroyed()) {
-            upstreamEventuate.consume(upstreamConsumer)
-            consuming = true
-        }
-    }
-
-    function upstreamConsumerRemoved () {
-        consuming = false
-        if (!eventuate.isDestroyed() && !upstreamEventuate.isDestroyed())
-            addUpstreamConsumer()
-    }
-
-    function removeUpstreamConsumer () {
-        upstreamEventuate.removeConsumer(upstreamConsumer)
-    }
-
     function produceError (err) {
         eventuate.produce(err instanceof Error ? err : new Error(err))
     }
-}
+})
